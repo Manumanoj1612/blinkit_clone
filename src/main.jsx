824ab4077bde5499fcf88db93fd888158817582e
MANU161103 <manumanoj1611@example.com>
2025-03-13 22:05:12 +0530

    second-version

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { store } from './redux/store'
-import { Provider } from 'react-redux'
+// import { store } from './redux/store'
+// import { Provider } from 'react-redux'
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <Provider>
     <App />
   </Provider>,
 )
