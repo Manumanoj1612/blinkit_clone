dff9290ac5be5b23ca10d341cd1da4e2994a4ac7
MANU161103 <manumanoj1611@example.com>
2025-03-13 22:37:10 +0530

    second-version

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 // import { store } from './redux/store'
 // import { Provider } from 'react-redux'
 createRoot(document.getElementById('root')).render(
-  <Provider>
+ 
     <App />
-  </Provider>,
+  
 )
